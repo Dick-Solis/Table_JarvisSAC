1da94ece2dd3d752abbc69ba91802335d90b3813
Dick-Solis <dicksolisromero@gmail.com>
2023-02-02 13:03:06 -0500

    Creación de CSS, reseteo de propiedades y configuracion de tipografía

--- scripts/pages/home-page.js
+++ scripts/pages/home-page.js
@@ -1,9 +1,43 @@
 import { STORE } from "../store.js";
 
-function render() {
-  console.log(STORE.users);
+function renderUsers() {
+  const users = STORE.users;
   return `
-    <h2>Hola Mundo</h2>
+    ${users.map(user => `
+      <tr>
+        <td>${users.indexOf(user) + 1}</td>
+        <td>${user.name.first}</td>
+        <td>${user.name.last}</td>
+        <td>${user.dob.age}</td>
+        <td>${user.gender === "female" ? "F" : "M"}</td>
+        <td>${user.email}</td>
+        <td>${user.nat}</td>
+        <td> <img src=${user.picture.thumbnail} alt=${user.name.first}></td>
+      </tr>`)}
+      `
+}
+
+function render() {
+
+  return `
+    <h2>Tabla Informativa de Usuarios Jarvis SAC</h2>
+    <table class="table">
+      <thead>
+        <tr>
+          <th>Nº</th>
+          <th>Nombre</th>
+          <th>Apellido</th>
+          <th>Edad</th>
+          <th>Género</th>
+          <th>Email</th>
+          <th>Nacionalidad</th>
+          <th>Foto</th>
+        </tr>
+      </thead>
+      <tbody>
+        ${renderUsers()}
+      </tbody>
+    </table>
   `
 }
 
