c68ea5110b856870c5fcd0f84e0e60cc75f95663
Dick-Solis <dicksolisromero@gmail.com>
2023-02-02 09:56:04 -0500

    consumo de api-fetch para traer los datos a la aplicación

--- scripts/config.js
+++ scripts/config.js
@@ -1,3 +1,2 @@
 // URL para traer 15 datos de API
 export const BASE_URI = "https://randomuser.me/api/?results=15";
-
---
+++ scripts/services/users.js
@@ -0,0 +1,5 @@
+import apiFetch from "./api-fetch";
+
+export async function getUsers() {
+  return await apiFetch();
+}
