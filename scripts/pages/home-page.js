824143b0e98cf4e01742542377d533293175d823
Dick-Solis <dicksolisromero@gmail.com>
2023-02-02 14:28:48 -0500

    mejorado de styles

--- scripts/pages/home-page.js
+++ scripts/pages/home-page.js
@@ -4,15 +4,15 @@ function renderUsers() {
   const users = STORE.users;
   return `
     ${users.map(user => `
-      <tr>
-        <td>${users.indexOf(user) + 1}</td>
+      <tr class="text-center">
+        <td >${users.indexOf(user) + 1}</td>
         <td>${user.name.first}</td>
         <td>${user.name.last}</td>
         <td>${user.dob.age}</td>
         <td>${user.gender === "female" ? "F" : "M"}</td>
         <td>${user.email}</td>
         <td>${user.nat}</td>
-        <td> <img src=${user.picture.thumbnail} alt=${user.name.first}></td>
+        <td> <img class="image-icon" src=${user.picture.thumbnail} alt=${user.name.first}></td>
       </tr>`)}
       `
 }
@@ -20,24 +20,26 @@ function renderUsers() {
 function render() {
 
   return `
-    <h2>Tabla Informativa de Usuarios Jarvis SAC</h2>
-    <table class="table">
-      <thead>
-        <tr>
-          <th>Nº</th>
-          <th>Nombre</th>
-          <th>Apellido</th>
-          <th>Edad</th>
-          <th>Género</th>
-          <th>Email</th>
-          <th>Nacionalidad</th>
-          <th>Foto</th>
-        </tr>
-      </thead>
-      <tbody>
-        ${renderUsers()}
-      </tbody>
-    </table>
+    <h2 class="title headin-lg text-center mb-8">Tabla Informativa de Usuarios Jarvis S.A.C. </h2>
+    <div class="container-table flex mb-8 content-lg">
+      <table>
+        <thead class="header-table">
+          <tr>
+            <th>Nº</th>
+            <th>Nombre</th>
+            <th>Apellido</th>
+            <th>Edad</th>
+            <th>Género</th>
+            <th>Email</th>
+            <th>Nacionalidad</th>
+            <th>Foto</th>
+          </tr>
+        </thead>
+        <tbody>
+          ${renderUsers()}
+        </tbody>
+      </table>
+    </div>
   `
 }
 
